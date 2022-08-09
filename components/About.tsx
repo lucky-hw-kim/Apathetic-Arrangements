44a011a4200f6f90aa783fd67713d81612c7f228
lucky <lucky.hw.kim@gmail.com>
2022-08-09 11:50:58 -0700

    alt modify

--- components/About.tsx
+++ components/About.tsx
@@ -18,13 +18,13 @@ const About = () => {
         Apathetic Arrangements is the perfect flower delivery service for people who wish to send passive-aggressive gifts to people they hate. You can choose from a variety of arrangements, including "Don't get well", "I don't give a damn", and "I couldn't give two fucks". Now you can show someone you hate that you really couldn't care less about them.
       </div>
       <div className={css.service}>
-        <div className={css.serviceLogo}><Image src={flower1} layout="responsive" />
+        <div className={css.serviceLogo}><Image alt="flower" src={flower1} layout="responsive" />
           <span>Designed with spite</span>
         </div>
-        <div className={css.serviceLogo}><Image src={flower2} layout="responsive" />
+        <div className={css.serviceLogo}><Image alt="flower2" src={flower2} layout="responsive" />
         <span>Arranged with care</span>
         </div>
-        <div className={css.serviceLogo}><Image src={flower3} layout="responsive" />
+        <div className={css.serviceLogo}><Image alt="flower3" src={flower3} layout="responsive" />
         <span>Deliver with speed</span>
         </div>
       </div>
